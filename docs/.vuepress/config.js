6db5af944eeb1e136d83521fa00b97b1ff4732f3
gdut-yy <gdut_yy@163.com>
2020-08-25 23:36:54 +0800

    add vuepress framework

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+// .vuepress/config.js
+module.exports = {
+  title: "《JavaScript 权威指南第七版》中文翻译",
+  base: "/doc-jstdg7/",
+  themeConfig: {
+    repo: "gdut-yy/JavaScript-The-Definitive-Guide-7th-zh",
+    repoLabel: "Github",
+    docsRepo: "gdut-yy/JavaScript-The-Definitive-Guide-7th-zh",
+    docsBranch: "master/docs",
+    editLinks: true,
+    editLinkText: "帮助我们改善此页面！",
+    lastUpdated: "Last Updated",
+    sidebarDepth: 2,
+    nav: [],
+    sidebar: {
+      "/": [
+        "",
+        "ch1.md",
+        "ch2.md",
+        "ch3.md",
+        "ch4.md",
+        "ch5.md",
+        "ch6.md",
+        "ch7.md",
+        "ch8.md",
+        "ch9.md",
+        "ch10.md",
+        "ch11.md",
+        "ch12.md",
+        "ch13.md",
+        "ch14.md",
+        "ch15.md",
+        "ch16.md",
+        "ch17.md",
+      ],
+    },
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,21 @@
+# 目录
+
+<img src="./cover.jpg" style="width: 28%" />
+
+- [第 1 章 JavaScript 概述](./ch1.md)
+- [第 2 章 词法结构](./ch2.md)
+- [第 3 章 类型、值和变量](./ch3.md)
+- [第 4 章 表达式和运算符](./ch4.md)
+- [第 5 章 语句](./ch5.md)
+- [第 6 章 对象](./ch6.md)
+- [第 7 章 数组](./ch7.md)
+- [第 8 章 函数](./ch8.md)
+- [第 9 章 类](./ch9.md)
+- [第 10 章 模块](./ch10.md)
+- [第 11 章 JavaScript 标准库](./ch11.md)
+- [第 12 章 迭代器和生成器](./ch12.md)
+- [第 13 章 异步 JavaScript](./ch13.md)
+- [第 14 章 元编程](./ch14.md)
+- [第 15 章 Web 浏览器中的 JavaScript](./ch15.md)
+- [第 16 章 服务器端 JavaScript](./ch16.md)
+- [第 17 章 JavaScript 工具和扩展](./ch17.md)
